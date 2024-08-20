b8195db96b3b2d1b0e7ada60220638a9d627dbf5
thomga <gabriel.thom02@gmail.com>
2024-08-20 14:53:34 -0300

    chore: add new paymaster test script

--- contracts/SimplePaymaster.sol
+++ contracts/SimplePaymaster.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@account-abstraction/contracts/core/BasePaymaster.sol";
 import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
 contract SimplePaymaster is BasePaymaster {
     constructor(address _entryPoint) BasePaymaster(IEntryPoint(_entryPoint)) {}
 
@@ -13,7 +14,8 @@ contract SimplePaymaster is BasePaymaster {
     ) internal view override returns (bytes memory context, uint256 preOpGas) {
         // Custom logic to validate the UserOperation
         // This example simply approves all operations
-        return ("", 0);
+        context = new bytes(0);
+        preOpGas = 0;
     }
 
     function _postOp(
